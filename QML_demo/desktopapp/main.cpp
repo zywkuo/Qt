bc630b64f14c757233d9f0f92ca9d727f7c64a9e
周艳伟 <zhouyanwei@sbr-info.com>
2023-06-12 11:11:25 +0800

    Qt 6.2

--- QML_demo/desktopapp/main.cpp
+++ QML_demo/desktopapp/main.cpp
@@ -2,17 +2,19 @@
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    // 修改风格 需添加头文件及CMakeList.txt引入
-    QQuickStyle::setStyle("Fusion");
-    QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/desktopapp/Main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+  // 修改风格 需添加头文件及CMakeList.txt引入
+  QQuickStyle::setStyle("Fusion");
+  QQmlApplicationEngine engine;
+  const QUrl url(u"qrc:/desktopapp/Main.qml"_qs);
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
